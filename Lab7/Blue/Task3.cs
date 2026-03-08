793741df4db448f841f8736229d53b3c0e6fc9a2
NikitaRoma <90859926+NikitaRoma@users.noreply.github.com>
2026-03-08 23:50:59 +0300

    Update Task3.cs

--- Lab7/Blue/Task3.cs
+++ Lab7/Blue/Task3.cs
@@ -1,6 +1,73 @@
-﻿namespace Lab7.Blue
+namespace Lab7.Blue
 {
     public class Task3
     {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private int[] _penaltytimes;
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[] PenaltyTimes => (int[])_penaltytimes.Clone();
+            public int TotalTime
+            {
+                get
+                {
+                    int sum = 0;
+                    foreach (int mark in _penaltytimes)
+                    {
+                        sum += mark;
+                    }
+                    return sum;
+                }
+            }
+            public bool IsExpelled
+            {
+                get
+                {
+                    foreach (int mark in _penaltytimes)
+                    {
+
+                        if (mark == 10)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+
+            public Participant(string Name, string Surname)
+            {
+                _name = Name;
+                _surname = Surname;
+                _penaltytimes = new int[0];
+            }
+            public void PlayMatch(int time)
+            {
+                if (time == 0 || time == 2 || time == 5 || time == 10)
+                {
+                    Array.Resize(ref _penaltytimes, _penaltytimes.Length + 1);
+                    _penaltytimes[_penaltytimes.Length - 1] = time;
+                }
+            }
+            public static void Sort(Participant[] array)
+            {
+                var sorted = array.OrderBy(p => p.TotalTime).ToArray();
+
+                Array.Copy(sorted, array, array.Length);
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {_name} Фамилия: {_surname}\n");
+                foreach (int i in _penaltytimes)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+
     }
 }
